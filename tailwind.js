8aad54fade32e7a47c37fcb29f51b51be270c54b
Stanley Zapata <jrszapata@hotmail.com>
2019-08-19 16:26:54 -0400

    Stanlix corporative site v2

---
+++ tailwind.js
@@ -0,0 +1,18 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        default: "var(--default-color)",
+        gray: "var(--gray-color)",
+        "gray-lightest": "var(--gray-lightest-color)",
+        "gray-darkest": "var(--gray-darkest-color)",
+        red: "var(--red-color)",
+        black: "var(--black-color)",
+        "brand-gray": "var(--brand-gray-color)",
+        "brand-orange": "var(--brand-orange-color)",
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
